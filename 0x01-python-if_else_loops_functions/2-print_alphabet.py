ecda62c66864dab475814ba8e40b09fb3ff594f6
Mogakamo <mogaka.amo254@gmail.com>
2021-09-07 21:35:23 +0300

    2 - alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def lowercaseAlphabet():
-    for c in range(97, 123):
-        print("{:c}".format(c), end="")
-    print("")
-lowercaseAlphabet()
+for c in range(97, 123):
+    print("{:c}".format(c), end="")
