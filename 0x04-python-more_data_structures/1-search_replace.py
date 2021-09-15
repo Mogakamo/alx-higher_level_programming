5b01392592ae863debd53abd687bbfb3293e0e20
Mogakamo <mogaka.amo254@gmail.com>
2021-09-15 10:35:03 -0700

    1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,10 @@
 
 def search_replace(my_list, search, replace):
     """ replaces all occurences of an element by another in a new list """
-    return (my_list.replace(search, replace))
+    new_list = []
+
+    for i in (len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+
+        return (new_list)
