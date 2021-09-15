95d5f4ae557745420c2683ec37903c3212a15e27
Mogakamo <mogaka.amo254@gmail.com>
2021-09-15 10:31:11 -0700

    commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    
+    """ replaces all occurences of an element by another in a new list """
+    return (my_list.replace(search, replace))
