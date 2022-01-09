f6624bc8b9ce0164dd69daf22ab98388dca0acd8
Mogakamo <mogaka.amo254@gmail.com>
2022-01-09 09:53:59 +0000

    commit

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Send a DELETE request to a given URL and display the response body.
-
-curl -sX DELETE "$1"
+# script that sends DELETE request to URL passed as first arg
+curl -s "$1" -X DELETE
