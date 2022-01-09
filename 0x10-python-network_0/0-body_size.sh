d259047befaad5ddc5d8dcaf2dc7bee3db54311e
Mogakamo <mogaka.amo254@gmail.com>
2022-01-09 10:12:34 +0000

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-curl -Is "$1" |grep "Content-Length:"| cut -d' ' -f2
+#!/bin/bash
+# Get The body  size  from outgoing  request to url introduced as argument
+curl -sI "$1" | grep 'Content-Length:' | cut -c 17-
